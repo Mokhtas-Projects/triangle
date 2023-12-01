57af482ec2639bbda4f035c541cd22a159f91248
nhussein2026 <naser738nas@gmail.com>
2023-12-01 16:13:45 +0300

    feat(sightsidebar): create right sidebar containing the layout of the stories and suggested freinds

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic':
 					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			},
+			colors: {
+				storyBg: '#FAFAFA',
+				storyPic: '#D9D9D9'
+			},
+			textColor: {
+				storiesHeadlineColor: '#706A6A'
 			}
 		}
 	},
