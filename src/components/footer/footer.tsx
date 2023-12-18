d231f37b006339df64ee45ad1f1d9367f71e0458
Amr Walidi <amr.nawaf@gmail.com>
2023-12-18 20:38:04 +0300

    fix(chat): add links to chat page and home page in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,9 @@ export const Footer = () => {
 	return (
 		<div className='fixed bottom-0 flex h-16 w-full items-center justify-center bg-teal-600 md:hidden'>
 			<ul className='flex gap-20 text-3xl text-gray-lighter'>
-				<IoMdHome />
+				<Link href='/'>
+					<IoMdHome />
+				</Link>
 				<IoGlobeOutline />
 				<IoVideocam />
 				<Link href='/chat'>
